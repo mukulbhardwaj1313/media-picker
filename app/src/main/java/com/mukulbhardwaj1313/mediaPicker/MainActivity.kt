2c4f124db8c32707070b7ddaa797b3bb615b8282
mukul <mukul.bhardwaj@cubastion.com>
2023-02-09 11:33:04 +0530

    second commit

--- app/src/main/java/com/mukulbhardwaj1313/mediaPicker/MainActivity.kt
+++ app/src/main/java/com/mukulbhardwaj1313/mediaPicker/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.mukulbhardwaj1313.mediaPicker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.mukulbhardwaj1313.library.ui.MediaOption
 import com.mukulbhardwaj1313.library.ui.MediaPicker
 
@@ -15,10 +17,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<View>(R.id.layout).setOnClickListener {
-            mediaPicker.actionGalleryVideoIntent({ path, source, type ->
-                Log.w(TAG, "onCreate: $path $source $type")
-            },mediaOption = MediaOption().setDuration(60000).setMaxVideoSize(20))
+            selectSource()
         }
 
+    }
+
+    private fun selectSource(){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("Select source")
+        builder.setItems(arrayOf<CharSequence>("Camera photo", "Camera Video", "Gallery Photo", "Gallery Video")) { _, which ->
+            when (which) {
+                0 -> {
+                    mediaPicker.actionCameraXPhotoIntent({ path, source, type ->
+                        Toast.makeText(this, "onCreate: $path $source $type", Toast.LENGTH_SHORT).show()
+                        Log.w(TAG, "onCreate: $path $source $type")
+                    })
+                }
+                1 -> {
+                    mediaPicker.actionCameraXVideoIntent({ path, source, type ->
+                        Toast.makeText(this, "onCreate: $path $source $type", Toast.LENGTH_SHORT).show()
+                        Log.w(TAG, "onCreate: $path $source $type")
+                    },mediaOption = MediaOption().setDuration(60000).setMaxVideoSize(20))
+                }
+                2 -> {
+                    mediaPicker.actionGalleryPhotoIntent({ path, source, type ->
+                        Toast.makeText(this, "onCreate: $path $source $type", Toast.LENGTH_SHORT).show()
+                        Log.w(TAG, "onCreate: $path $source $type")
+                    })
+                }
+                3 -> {
+                    mediaPicker.actionGalleryVideoIntent({ path, source, type ->
+                        Toast.makeText(this, "onCreate: $path $source $type", Toast.LENGTH_SHORT).show()
+                        Log.w(TAG, "onCreate: $path $source $type")
+                    },mediaOption = MediaOption().setDuration(160000).setMaxVideoSize(25))
+                }
+            }
+        }
+        builder.create().show()
     }
 }
